354f52551f9091dd0cad93b1fa2fc45d2b1c77a0
mathFonseca <matheus.2016@alunos.utfpr.edu.br>
2019-09-09 12:19:02 -0300

    Updating LAB 2. Message 1 works, 2 and 3 needs verification

--- Laboratório 2 - Maria e Matheus F/LAB 2 - Matheus e Maria (07 09)/Exemplo_UTFPR_TM4C1294XL_BoosterPackMKII/ProjectFiles/exemploTIVA.c
+++ Laboratório 2 - Maria e Matheus F/LAB 2 - Matheus e Maria (07 09)/Exemplo_UTFPR_TM4C1294XL_BoosterPackMKII/ProjectFiles/exemploTIVA.c
@@ -51,9 +51,14 @@ bool f_decodedMessage = false;		// Alterada para TRUE quando t_messageDecode ter
 bool f_beforePrimeTest = false;		// Alterada para TRUE quando t_lastWordTeste termina de testar a chave
 bool f_afterPrimeTest = false;		// Alterada para TRUE quando t_penultimateWordTeste termina de testar a chave
 
+bool t_ultimoPrimo = false;				// Alterada para TRUE quando o teste do ultimo primo der certo
+bool t_penultimoPrimo = false;		// Alterada para TRUE quando o teste do ultimo primo der certo
+
 uint32_t key = 3;
 uint32_t lastKey = 2;
 uint32_t nextKey = 5;
+
+uint8_t men[MSG_LENGTH];
 
 char msg[MSG_LENGTH/2 - 2], pbuf[10];
 
@@ -152,6 +157,14 @@ static void floatToString(float value, char *pBuf, uint32_t len, uint32_t base, 
 /*----------------------------------------------------------------------------
  *    Initializations
  *---------------------------------------------------------------------------*/
+void InitializeMessege(){
+	uint8_t i;
+	
+	for(i=0; i<MSG_LENGTH; i++){
+		men[i] = men2[i];
+	}
+
+}
 void init_display(){
 	uint8_t i;
 	GrContextInit(&sContext, &g_sCfaf128x128x16);
@@ -178,7 +191,6 @@ void init_all(){
 	SysTick_Init();
 	init_display();
 }
-
 void draw_circle(uint16_t x, uint16_t y){
 	GrCircleDraw(&sContext, 
 		(sContext.psFont->ui8MaxWidth)*x + (sContext.psFont->ui8MaxWidth)/2, 
@@ -224,120 +236,7 @@ uint32_t saturate(uint8_t r, uint8_t g, uint8_t b){
 					(((uint32_t) g) <<  8) | 
 					( (uint32_t) b       );
 }
-/*----------------------------------------------------------------------------
- *    Threads
- *---------------------------------------------------------------------------*/
-void t_keyGenerator(void const *argument){
-	// Executa após messagePrint termina de executar.
-	if(f_keyGenerator == false)
-	{
-		
-		// Da yield quando termina	
-		f_keyGenerator = true;
-	}
-	else
-	{
-		// Da yield se não pode executar;
-	}
-}
 
-
-void t_messageDecode(void const *argument){
-	if(f_keyGenerator == true && f_decodedMessage == false)
-	{
-		// Decodifica a mensagem
-		uint16_t i=0;
-		uint16_t par=0;	// Idenficar índice da mensagem (Par / Ímpar)
-		uint16_t m_par = (key - lastKey);	// Indices pares: Chave  - Primo Anterior
-		uint16_t m_impar = (key - nextKey);	// Indices ímpares: Chave - Próximo Primo
-		uint32_t decodedMsg[MSG_LENGTH/2];	// 68 posições. Cada letra ocupa 2 espaços, 34 letras ao todo.
-		
-		for(i = 0; i < MSG_LENGTH; i += 2)	// For que decodifica a mensagem
-		{
-			uint32_t word;	// Porque a declaração é dentro do for? Realmente necessário?
-			word = men1[i];
-			word += men1[i+1] * 16 * 16;
-			
-			if(par % 2 ==0)	// Se índice par, divide por m_par
-			{	
-				word /= m_par;
-			}
-			else	// Se índice ímpar, divide por m_impar
-			{	
-				/* Necessário tratar overflow aqui*/
-				word = 0x0020;	// ASCII 0x20 é Space
-			}											
-			par++;
-			decodedMsg[i/2] = word;
-		}
-		
-		for(i = 0; i < MSG_LENGTH/2 - 2; i++)	// Transfere a mensagem decodificada para vetor mensagem 
-		{
-			msg[i] = (char)(decodedMsg[i]);
-		}			
-	
-		f_decodedMessage = true;
-		// Da yield quando termina
-	}
-	else
-	{
-		// Da yield se não pode executar;
-	}
-}
-
-
-void t_lastWordTest(void const *argument){
-	if(f_keyGenerator == true && f_afterPrimeTest == false)
-	{
-		// Testa a chave
-		// Da yield quando termina	
-		f_afterPrimeTest = true;
-	}
-	else
-	{
-		// Da yield se não pode executar;
-	}
-}
-
-
-void t_penultimateWordTest(void const *argument){
-	if(f_keyGenerator == true && f_beforePrimeTest == false)
-	{
-		// Testa a chave
-		// Da yield quando termina	
-		f_beforePrimeTest = true;
-	}
-	else
-	{
-		// Da yield se não pode executar;
-	}
-}
-
-void t_messagePrint(void const *argument){
-	// Checa se as threads necessárias podem executar ou não.
-	if( f_decodedMessage == true && f_beforePrimeTest == true && f_afterPrimeTest == true && f_keyGenerator == true)
-	{
-		// Imprime a mensagem na tela
-		uint16_t i = 0;
-		uint16_t displayLines = (MSG_LENGTH/2)/21 + 1;
-		for(i = 0; i < displayLines; i++)					// Imprime a mensagem  
-		{
-			GrStringDraw(&sContext, msg + (i*21), 21, 0, (sContext.psFont->ui8Height+2)*i, true);
-		}
-		// Ao terminar, reseta as flags para false e dá yield.
-		
-		f_decodedMessage = false;
-		f_beforePrimeTest = false;
-		f_afterPrimeTest = false;
-		f_keyGenerator  = false;
-		
-		
-	}
-	else
-	{
-		// Não pode executar ainda. Da yield
-	}
-}
 
 
 bool isPrime(uint32_t next){
@@ -356,6 +255,226 @@ bool isPrime(uint32_t next){
 	return !divide; //se divide for false, eh um primo
 }
 
+/*----------------------------------------------------------------------------
+ *    Threads
+ *---------------------------------------------------------------------------*/
+void t_keyGenerator(void const *argument){
+	while(true){
+		// Executa após messagePrint termina de executar.
+		if(f_keyGenerator == false)
+		{
+			// Procura um primo
+			uint8_t i=0;
+			bool achou = false;
+			bool divide = false;
+			uint32_t next=0;
+			
+			lastKey = key;
+			key = nextKey;
+			next = nextKey+1;
+			
+			while(!achou)
+			{	//find next prime
+				
+				// Checa se é primo pela func isPrime(next)			
+				if(!isPrime(next))
+				{
+					next++;
+				}
+				else{
+					achou = true;
+				}
+			}
+			nextKey = next;
+			
+			//chave 13399
+			//anterior 13397
+			//proxima 13411
+			key = 13399;
+			lastKey = 13397;
+			nextKey = 13411;
+			
+			// Da yield quando termina	
+			f_keyGenerator = true;
+			osThreadYield();
+		}
+		else
+		{
+			// Da yield se não pode executar;
+			osThreadYield();
+		}
+	}
+}
+
+
+void t_messageDecode(void const *argument){
+	while(true){
+			if(f_keyGenerator == true && f_decodedMessage == false)
+			{
+				// Decodifica a mensagem
+				uint16_t i=0;
+				uint16_t par=0;	// Idenficar índice da mensagem (Par / Ímpar)
+				uint16_t m_par = (key - lastKey);	// Indices pares: Chave  - Primo Anterior
+				uint8_t m_impar = nextKey - key;	// Indices ímpares: Chave - Próximo Primo
+				uint32_t decodedMsg[MSG_LENGTH/2];	// 68 posições. Cada letra ocupa 2 espaços, 34 letras ao todo.
+				
+				for(i = 0; i < MSG_LENGTH; i += 2)	// For que decodifica a mensagem
+				{
+					uint16_t word;	// Porque a declaração é dentro do for? Realmente necessário?
+					word = men[i];
+					word += men[i+1] * 16 * 16;
+					
+					if(par % 2 ==0)	// Se índice par, divide por m_par
+					{	
+						word /= m_par;
+					}
+					else	// Se índice ímpar, divide por m_impar
+					{	
+						/* Necessário tratar overflow aqui*/
+						word = (word/(-m_impar));
+						//word = word % 0xff;
+						//word = 0x20;
+					}											
+					par++;
+					decodedMsg[i/2] = word;
+				}
+				
+				for(i = 0; i < MSG_LENGTH/2 - 4; i++)	// Transfere a mensagem decodificada para vetor mensagem 
+				{
+					msg[i] = (char)(decodedMsg[i]);
+				}			
+			
+				f_decodedMessage = true;
+				// Da yield quando termina
+				osThreadYield();
+			}
+			else
+			{
+				// Da yield se não pode executar;
+				osThreadYield();
+			}
+	}
+}
+
+void t_lastWordTest(void const *argument){
+	while(!t_ultimoPrimo){
+		if(f_keyGenerator == true && f_afterPrimeTest == false)
+		{
+			// Testa a chave
+			char pbufa[10],pbufb[10],pbufc[10],pbufd[10],pbufe[10];
+			
+			uint32_t ultima = men[MSG_LENGTH-4];
+			
+			ultima+= men[MSG_LENGTH-3] * 16 * 16;
+			ultima+= men[MSG_LENGTH-2] * 16 * 16 * 16 * 16;
+			ultima+= men[MSG_LENGTH-1] * 16 * 16 * 16 * 16 * 16 * 16;
+
+			if(key*lastKey == ultima)
+					t_ultimoPrimo =true;	
+			// Da yield quando termina	
+			f_afterPrimeTest = true;
+			osThreadYield();
+		}
+		else
+		{
+			// Da yield se não pode executar;
+			osThreadYield();
+		}
+	}
+}
+
+
+void t_penultimateWordTest(void const *argument){
+	while(!t_penultimoPrimo){
+		if(f_keyGenerator == true && f_beforePrimeTest == false)
+		{
+			// Testa a chave
+			char pbufa[10];
+		
+			uint32_t p_ultima = men[MSG_LENGTH-8];
+			p_ultima+= men[MSG_LENGTH-7] * 16 * 16;
+			p_ultima+= men[MSG_LENGTH-6] * 16 * 16 * 16 * 16;
+			p_ultima+= men[MSG_LENGTH-5] * 16 * 16 * 16 * 16 * 16 * 16;
+			
+			if(key*nextKey == p_ultima){
+					t_penultimoPrimo =true;
+			}
+			
+			intToString(key, pbufa, 10, 10, 3);
+			
+			GrStringDraw(&sContext, "Chave: ", -1, 0, (sContext.psFont->ui8Height+2)*5, true);
+			GrStringDraw(&sContext, (char*)pbufa, -1, (sContext.psFont->ui8MaxWidth)*7, (sContext.psFont->ui8Height+2)*5, true);
+			
+			// Da yield quando termina
+
+			f_beforePrimeTest = true;
+			osThreadYield();			
+		}
+		else
+		{
+			osThreadYield();
+			// Da yield se não pode executar;
+		}
+	}
+}
+
+void t_messagePrint(void const *argument){
+	while(true){
+		// Checa se as threads necessárias podem executar ou não.
+		if( f_decodedMessage == true && f_beforePrimeTest == true && f_afterPrimeTest == true && f_keyGenerator == true)
+		{
+			// Imprime a mensagem na tela
+			uint16_t i = 0;
+			uint16_t displayLines = (MSG_LENGTH/2)/21 + 1;
+			for(i = 0; i < displayLines; i++)					// Imprime a mensagem  
+			{
+				GrStringDraw(&sContext, msg + (i*21), 21, 0, (sContext.psFont->ui8Height+2)*i, true);
+			}
+			// Ao terminar, reseta as flags para false e dá yield.
+			
+			f_decodedMessage = false;
+			f_beforePrimeTest = false;
+			f_afterPrimeTest = false;
+			f_keyGenerator  = false;
+			
+			osThreadYield();
+			
+		}
+		else
+		{
+			// Não pode executar ainda. Da yield
+			osThreadYield();
+		}
+	}
+}
+
+
+
+// Threads
+osThreadDef(t_keyGenerator, osPriorityNormal, 1, 0); 
+osThreadDef(t_messageDecode, osPriorityNormal, 1, 0); 
+osThreadDef(t_lastWordTest, osPriorityNormal, 1, 0); 
+osThreadDef(t_penultimateWordTest, osPriorityNormal, 1, 0); 
+osThreadDef(t_messagePrint, osPriorityNormal, 1, 0); 
+
+int main(){
+	osKernelInitialize();
+  //Initializing all peripherals
+	init_all();
+		
+	// Cria as Thread
+	InitializeMessege();
+	osThreadCreate(osThread(t_keyGenerator), NULL);
+	osThreadCreate(osThread(t_messageDecode), NULL);
+	osThreadCreate(osThread(t_lastWordTest), NULL);
+	osThreadCreate(osThread(t_penultimateWordTest), NULL);
+	osThreadCreate(osThread(t_messagePrint), NULL);
+	
+	osKernelStart(); 
+
+}
+
+/*
 void findNextPrime(){
 	uint8_t i=0;
 	bool achou = false;
@@ -382,16 +501,16 @@ void findKey(){
 	
 	bool achou = false;
 	
-	uint32_t ultima = men1[MSG_LENGTH-4];
-	uint32_t p_ultima = men1[MSG_LENGTH-8];
+	uint32_t ultima = men[MSG_LENGTH-4];
+	uint32_t p_ultima = men[MSG_LENGTH-8];
 
-	ultima+= men1[MSG_LENGTH-3] * 16 * 16;
-	ultima+= men1[MSG_LENGTH-2] * 16 * 16 * 16 * 16;
-	ultima+= men1[MSG_LENGTH-1] * 16 * 16 * 16 * 16 * 16 * 16;
+	ultima+= men[MSG_LENGTH-3] * 16 * 16;
+	ultima+= men[MSG_LENGTH-2] * 16 * 16 * 16 * 16;
+	ultima+= men[MSG_LENGTH-1] * 16 * 16 * 16 * 16 * 16 * 16;
 
-	p_ultima+= men1[MSG_LENGTH-7] * 16 * 16;
-	p_ultima+= men1[MSG_LENGTH-6] * 16 * 16 * 16 * 16;
-	p_ultima+= men1[MSG_LENGTH-5] * 16 * 16 * 16 * 16 * 16 * 16;
+	p_ultima+= men[MSG_LENGTH-7] * 16 * 16;
+	p_ultima+= men[MSG_LENGTH-6] * 16 * 16 * 16 * 16;
+	p_ultima+= men[MSG_LENGTH-5] * 16 * 16 * 16 * 16 * 16 * 16;
 	
 	intToString(ultima, pbufd, 10, 10, 3);
 	intToString(p_ultima, pbufe, 10, 10, 3);
@@ -420,8 +539,6 @@ void findKey(){
 			GrStringDraw(&sContext, (char*)pbufc, -1, (sContext.psFont->ui8MaxWidth)*7, (sContext.psFont->ui8Height+2)*7, true);
 	}
 }
-
-
 int showMessage (void) {
 	uint16_t i=0;
 	uint16_t par=0;														// Idenficar índice da mensagem (Par / Ímpar)
@@ -430,13 +547,12 @@ int showMessage (void) {
 	uint32_t decodedMsg[MSG_LENGTH/2];				// Não entendi pq /2
 	int displayLines = (MSG_LENGTH/2)/21 + 1;
   //Initializing all peripherals
-	init_all();
-	
+		
 	for(i = 0; i < MSG_LENGTH; i += 2)				// For que decodifica a mensagem
 	{
-		uint32_t word;
-		word = men1[i];
-		word += men1[i+1] * 16 * 16;
+		int16_t word;
+		word = men[i];
+		word += men[i+1] * 16 * 16;
 		
 		if(par % 2 ==0) 												// Se índice par, divide por m_par
 		{	
@@ -444,8 +560,8 @@ int showMessage (void) {
 		}
 		else																		 // Se índice ímpar, divide por m_impar
 		{	
-			/* Necessário tratar overflow aqui*/
-			word = 0x0020;
+			word /= m_impar; 
+			//word-=0xFFFFFFFFFFFFEB00;
 		}											
 		par++;
 		decodedMsg[i/2] = word;
@@ -470,156 +586,5 @@ int main2(){
 	  osDelay(osWaitForever);
 		return 0;
 }
-
-// Threads
-osThreadDef(t_keyGenerator, osPriorityNormal, 1, 0); 
-osThreadDef(t_messageDecode, osPriorityNormal, 1, 0); 
-osThreadDef(t_lastWordTest, osPriorityNormal, 1, 0); 
-osThreadDef(t_penultimateWordTest, osPriorityNormal, 1, 0); 
-osThreadDef(t_messagePrint, osPriorityNormal, 1, 0); 
-
-int main(){
-	
-  //Initializing all peripherals
-	init_all();
-
-	osKernelInitialize();
-	
-	// Cria as Thread
-	
-	osThreadCreate(osThread(t_keyGenerator), NULL);
-	osThreadCreate(osThread(t_messageDecode), NULL);
-	osThreadCreate(osThread(t_lastWordTest), NULL);
-	osThreadCreate(osThread(t_penultimateWordTest), NULL);
-	osThreadCreate(osThread(t_messagePrint), NULL);
-	
-	osKernelStart(); 
-	osDelay(osWaitForever);
-	
-	return 0;
-}
-
-int main1 (void) {
-
-  char pbufx[10], pbufy[10], pbufz[10];
-	bool center;
-	float temp,lux;
-	float mic;
-	bool s1_press, s2_press;
-	uint8_t  	r, g, b;
-	uint32_t color;
-	uint16_t x, y, z, angle=0;
-	
-	//Initializing all peripherals
-	init_all();
-	//Sidelong menu creation
-	//init_sidelong_menu();
-
-  while(1){
-		
-/*  Acelerometro		*/
-			x = accel_read_x();
-			y = accel_read_y();
-			z = accel_read_z();
-			
-			floatToString((x*3.30/0xFFF), pbufx, 10, 10, 1, 2);
-			floatToString((y*3.30/0xFFF), pbufy, 10, 10, 1, 2);		
-			floatToString((z*3.30/0xFFF), pbufz, 10, 10, 1, 2);
-					
-			GrContextBackgroundSet(&sContext, ClrBlack);
-			GrContextForegroundSet(&sContext, ClrMagenta);
-			GrStringDraw(&sContext,(char*)pbufx, -1, (sContext.psFont->ui8MaxWidth)*6,  (sContext.psFont->ui8Height+2)*3, true);
-			GrContextForegroundSet(&sContext, ClrYellow);
-			GrStringDraw(&sContext,(char*)pbufy, -1, (sContext.psFont->ui8MaxWidth)*11, (sContext.psFont->ui8Height+2)*3, true);
-			GrContextForegroundSet(&sContext, ClrCyan);
-			GrStringDraw(&sContext,(char*)pbufz, -1, (sContext.psFont->ui8MaxWidth)*16, (sContext.psFont->ui8Height+2)*3, true);
-
-/*	RGB 	*/
-			r = x*0xFF/0xFFF;   		// dados do acelerometro
-			g = y*0xFF/0xFFF;
-			b = z*0xFF/0xFFF;
-		
-			intToString(r, pbufx, 10, 10, 3);
-			intToString(g, pbufy, 10, 10, 3);
-			intToString(b, pbufz, 10, 10, 3);
-			
-			color = saturate(r,g,b);
-			color = rgb_color_intensity(color, 0.4);
-			rgb_write_color(color);
-
-   		GrContextBackgroundSet(&sContext, ClrBlack);
-			GrContextForegroundSet(&sContext, ClrRed);
-			GrStringDraw(&sContext,(char*)pbufx, -1, (sContext.psFont->ui8MaxWidth)*6,  (sContext.psFont->ui8Height+2)*2, true);
-			GrContextForegroundSet(&sContext, ClrGreen);
-			GrStringDraw(&sContext,(char*)pbufy, -1, (sContext.psFont->ui8MaxWidth)*10, (sContext.psFont->ui8Height+2)*2, true);
-			GrContextForegroundSet(&sContext, ClrBlue);
-			GrStringDraw(&sContext,(char*)pbufz, -1, (sContext.psFont->ui8MaxWidth)*14, (sContext.psFont->ui8Height+2)*2, true);
-
-/*  Joystick		*/		
-			x = joy_read_x();
-			y = joy_read_y();
-			center = joy_read_center();
-
-			intToString(x*200/0xFFF-100, pbufx, 10, 10, 4);
-			intToString(y*200/0xFFF-100, pbufy, 10, 10, 4);
-			
-			if (center)
-				intToString(1, pbufz, 10, 10, 1);
-			else
-				intToString(0, pbufz, 10, 10, 1);
-				
-			GrContextBackgroundSet(&sContext, ClrBlack);
-			GrContextForegroundSet(&sContext, ClrWhite);
-			GrStringDraw(&sContext,(char*)pbufx, -1, (sContext.psFont->ui8MaxWidth)*6,  (sContext.psFont->ui8Height+2)*7, true);
-			GrStringDraw(&sContext,(char*)pbufy, -1,  (sContext.psFont->ui8MaxWidth)*11, (sContext.psFont->ui8Height+2)*7, true);
-			GrStringDraw(&sContext,(char*)pbufz, -1,  (sContext.psFont->ui8MaxWidth)*18, (sContext.psFont->ui8Height+2)*7, true);
-			
-/*  Temperatura		*/		
-			temp = temp_read_celsius();
-			floatToString(temp, pbufx, 10, 10, 2, 3);
-		
-			GrContextBackgroundSet(&sContext, ClrBlack);
-			GrContextForegroundSet(&sContext, ClrWhite);
-			GrStringDraw(&sContext,(char*)pbufx, -1, (sContext.psFont->ui8MaxWidth)*6,  (sContext.psFont->ui8Height+2)*4, true);
-
-/*	Light			*/
-	
-			lux = opt_fread_lux();
-		
-			floatToString(lux, pbufx, 10, 10, 4, 3);
-		
-			GrContextBackgroundSet(&sContext, ClrBlack);
-			GrContextForegroundSet(&sContext, ClrWhite);
-			GrStringDraw(&sContext,(char*)pbufx, -1, (sContext.psFont->ui8MaxWidth)*6,  (sContext.psFont->ui8Height+2)*5, true);
-	
-/*	Microfone 	*/
-			mic = mic_norm();
-		
-			intToString((int32_t) (mic*200-100), pbufx, 10, 10, 4);
-
-			GrContextBackgroundSet(&sContext, ClrBlack);
-			GrContextForegroundSet(&sContext, ClrWhite);
-			GrStringDraw(&sContext,(char*)pbufx, -1, (sContext.psFont->ui8MaxWidth)*6,  (sContext.psFont->ui8Height+2)*6, true);
-
-/*	Botoes 	*/			
-			s1_press = button_read_s1();
-			s2_press = button_read_s2();
-
-			if (s1_press)
-				intToString(1, pbufx, 10, 10, 1);
-			else
-				intToString(0, pbufx, 10, 10, 1);
-
-			if (s2_press)
-				intToString(1, pbufy, 10, 10, 1);
-			else
-				intToString(0, pbufy, 10, 10, 1);
-
-			GrContextBackgroundSet(&sContext, ClrBlack);
-			GrContextForegroundSet(&sContext, ClrWhite);
-			GrStringDraw(&sContext,(char*)pbufx, -1, (sContext.psFont->ui8MaxWidth)*6,  (sContext.psFont->ui8Height+2)*8, true);
-			GrStringDraw(&sContext,(char*)pbufy, -1,  (sContext.psFont->ui8MaxWidth)*11, (sContext.psFont->ui8Height+2)*8, true);
-			
-	}	
-}
+*/
 

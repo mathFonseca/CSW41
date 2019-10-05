2b9e61e09a1496ec809bea7a67436017bcee2f24
mathFonseca <matheus.2016@alunos.utfpr.edu.br>
2019-10-05 16:28:32 -0300

    Threads and Mutexes
    Iniciando programação final do projeto baseado em Threads e Mutexes

---
+++ Laboratório 3 - Maria e Matheus F/Versão com Boss e Vidas adicionadas/_Exemplo_UTFPR_TM4C1294XL_BoosterPackMKII/ProjectFiles/point.h
@@ -0,0 +1,10 @@
+#ifndef POINT_H
+#define POINT_H
+
+extern const unsigned long point_termination;
+extern const unsigned long point_start;
+extern const unsigned long point_finish;
+extern const unsigned long point_length;
+extern const unsigned char point[];
+
+#endif /* POINT_H */
